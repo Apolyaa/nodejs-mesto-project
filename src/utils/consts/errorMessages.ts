a6ebef35f3261b0a80d3d137b9ee2104419a7634
Apolya <polinagolubeva02@gmail.com>
2025-07-06 18:48:16 +0300

    fix error message

--- src/utils/consts/errorMessages.ts
+++ src/utils/consts/errorMessages.ts
@@ -1,5 +1,5 @@
 const ERROR_MESSAGES = {
-  internalError: 'Произошла ошибка.',
+  internalError: 'На сервере произошла ошибка',
   notValid: 'Переданы некорректные данные.',
   userNotFound: 'Пользователь не найден.',
   cardNotFound: 'Карточка не найдена.',
